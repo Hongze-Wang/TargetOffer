f498a3867af1c7ad59069a203719afbb92e5ecfd
Steve Wang <wanghongzecn@qq.com>
2019-09-17 16:48:13 +0800

    Update 29最小的k个数.java

--- 29最小的k个数.java
+++ 29最小的k个数.java
@@ -16,3 +16,39 @@ public class Solution {
         return res;
     }
 }
+
+// import java.util.ArrayList;
+// import java.util.Comparator;
+// import java.util.PriorityQueue;
+
+// public class Solution {
+//     public ArrayList<Integer> GetLeastNumbers_Solution(int[] input, int k) {
+//         if(input == null || k < 0 || input.length == 0 || input.length < k) {
+//             return new ArrayList<>();
+//         }
+//         PriorityQueue<Integer> bigHeap = new PriorityQueue<>(new MyComparator());
+//         for(int num : input) {
+//             if(bigHeap.size() < k) {
+//                 bigHeap.add(num);
+//             } else {
+//                 if(num < bigHeap.peek()) {
+//                     bigHeap.poll();
+//                     bigHeap.add(num);
+//                 }
+//             }
+//         }
+//         ArrayList<Integer> res = new ArrayList();
+//         while(!bigHeap.isEmpty()) {
+//             res.add(bigHeap.poll());
+//         }
+//         return res;
+//     }
+// }
+
+// class MyComparator implements Comparator<Integer> {
+    
+//     @Override
+//     public int compare(Integer o1, Integer o2) {
+//         return o2 - o1;
+//     }
+// }
