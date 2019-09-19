62c30fb1c208be6ec5bf955e061a3856cd8a2e99
Steve Wang <wanghongzecn@qq.com>
2019-09-19 21:50:49 +0800

    Update 45扑克牌顺子.java

--- 45扑克牌顺子.java
+++ 45扑克牌顺子.java
@@ -1,17 +1,22 @@
-import java.util.ArrayList;
-import java.util.Collections;
+// two conditions
+// 1. no replica
+// 2. max - min <= 5
 
 public class Solution {
-    public boolean isContinuous(int [] numbers) {
-        if(numbers == null || numbers.length == 0) return false;
-        ArrayList<Integer> list = new ArrayList();
-        int n = 0;
+    public boolean isContinuous(int[] numbers) {
+        if(numbers == null || numbers.length != 5) return false;
+        int min = 14;
+        int max = -1;
+        int flag = 0;
         for(int i = 0; i < numbers.length; i++) {
-            if(numbers[i] == 0) n++;
-            else list.add(numbers[i]);
+            if(numbers[i] < 0 || numbers[i] > 13) return false;
+            if(numbers[i] == 0) continue;
+            if(((flag >> numbers[i]) & 1) == 1) return false;
+            flag |= (1 << numbers[i]);
+            if(numbers[i] < min) min = numbers[i];
+            if(numbers[i] > max) max = numbers[i];
         }
-        Collections.sort(list);
-        int diff = list.get(list.size()-1) - list.get(0);
-        return diff <= 4  && 4 - diff <= n; 
+        if(max - min > 4) return false;
+        return true;
     }
 }
