17e31f30e6e5d0fb3c8421b0d7356479880ecd70
Steve Wang <wanghongzecn@qq.com>
2019-09-19 10:15:58 +0800

    Update 40数组中只出现一次的数字(2个).java

---
+++ 40数组中只出现一次的数字(2个).java
@@ -0,0 +1,75 @@
+//num1,num2分别为长度为1的数组。传出参数
+//将num1[0],num2[0]设置为返回结果
+
+
+// 排序 然后使用栈
+// public class Solution {
+//     public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+//         Arrays.sort(array);
+        
+//         Stack<Integer> stack = new Stack(); // Also can use a ArraList as a Stack more faster
+        
+//         for(int i = 0; i < array.length; i++) {
+//             if(stack.empty() || stack.peek() != array[i]) {
+//                 stack.push(array[i]);
+//             } else if(stack.peek() == array[i]) {
+//                 stack.pop();
+//             }
+//         }
+//         num1[0] = stack.pop();
+//         num2[0] = stack.pop();
+//     }
+// }
+
+// HashMap
+
+// import java.util.HashMap;
+
+// public class Solution {
+//     public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+//         HashMap<Integer, Integer> map = new HashMap();
+//         for(int i = 0; i < array.length; i++) {
+//             if(map.containsKey(array[i])) {
+//                 map.put(array[i], 2);
+//             } else {
+//                 map.put(array[i], 1);
+//             }
+//         }
+//         int flag = 0;
+//         for(int i = 0; i < array.length; i++) {
+//             if(map.get(array[i]) == 1) {
+//                 if(flag == 0) {
+//                     num1[0] = array[i];
+//                     flag = 1;
+//                 } else {
+//                     num2[0] = array[i];
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Using XOR
+
+public class Solution {
+    public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
+        int xor1 = 0;
+        for(int i = 0; i < array.length; i++) {
+            xor1 = xor1 ^ array[i];
+        }
+        int index = 1;
+        while((index & xor1) == 0) {
+            index = index << 1;
+        }
+        int res1 = 0, res2 = 0;
+        for(int i = 0; i < array.length; i++) {
+            if((index & array[i]) == 0) {
+                res1 = res1 ^ array[i];
+            } else {
+                res2 = res2 ^ array[i];
+            }
+        }
+        num1[0] = res1;
+        num2[0] = res2;
+    }
+}
