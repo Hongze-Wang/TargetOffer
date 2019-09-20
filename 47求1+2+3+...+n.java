75a366495765f137cb3cdf2c8acb3e8ff6931f68
Steve Wang <wanghongzecn@qq.com>
2019-09-20 18:57:02 +0800

    Update 47求1+2+3+...+n.java

--- 47求1+2+3+...+n.java
+++ 47求1+2+3+...+n.java
@@ -1,5 +1,15 @@
+// public class Solution {
+//     public int Sum_Solution(int n) {
+//         return (1+n)*n / 2;
+//     }
+// }
+
 public class Solution {
     public int Sum_Solution(int n) {
-        return (1+n)*n / 2;
+        int sum = 0;
+        for(int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
     }
 }
