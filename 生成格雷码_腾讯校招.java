5c6b87f80c92b03a476cdf8bd4f178aa408ee353
Steve Wang <wanghongzecn@qq.com>
2020-07-11 16:19:48 +0800

    Update 生成格雷码_腾讯校招.java

--- 生成格雷码_腾讯校招.java
+++ 生成格雷码_腾讯校招.java
@@ -14,17 +14,30 @@
 // 1、n=2的格雷码是在n=1的格雷码按顺序在前面加0,后面加1
 // 2、n时的格雷码的长度是n-1时的格雷码的长度的两倍
 
+// class Solution {
+//     public String[] getGreyCode(int n) {
+//         if(n == 1) return new String[] {"0","1"};
+//         String[] temp = getGreyCode(n - 1);
+//         String[] result = new String[temp.length * 2];
+//         for(int i = 0; i < temp.length; i++) {
+//             result[i] = "0" + temp[i];
+//         }
+//         for(int i = 0; i < temp.length; i++) {
+//             result[i + temp.length] = "1" + temp[temp.length - i -1];
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public String[] getGreyCode(int n) {
-        if(n == 1) return new String[] {"0","1"};
-        String[] temp = getGreyCode(n - 1);
-        String[] result = new String[temp.length * 2];
-        for(int i = 0; i < temp.length; i++) {
-            result[i] = "0" + temp[i];
+        if(n == 1) return {"0", "1"};
+        String[] temp = getGreyCode(n-1);
+        String[] res = new String[temp.length*2];
+        for(int i=0; i<temp.length; i++) {
+            res[i] = "0" + temp[i];
+            res[i+temp.length] = "1" + temp[temp.length-i-1];
         }
-        for(int i = 0; i < temp.length; i++) {
-            result[i + temp.length] = "1" + temp[temp.length - i -1];
-        }
-        return result;
+        return res;
     }
 }
