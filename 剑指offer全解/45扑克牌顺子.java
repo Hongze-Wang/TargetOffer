4f546458fab0acf6e1a3aa2429fa250b5a1a4a46
Steve Wang <wanghongzecn@qq.com>
2020-08-28 10:23:28 +0800

    Create 45扑克牌顺子.java

---
+++ 剑指offer全解/45扑克牌顺子.java
@@ -0,0 +1,22 @@
+// two conditions
+// 1. no replica
+// 2. max - min > 4
+
+public class Solution {
+    public boolean isContinuous(int[] numbers) {
+        if(numbers == null || numbers.length != 5) return false;
+        int min = 14;
+        int max = -1;
+        int flag = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] < 0 || numbers[i] > 13) return false;
+            if(numbers[i] == 0) continue;
+            if(((flag >> numbers[i]) & 1) == 1) return false;
+            flag |= (1 << numbers[i]);
+            if(numbers[i] < min) min = numbers[i];
+            if(numbers[i] > max) max = numbers[i];
+        }
+        if(max - min > 4) return false;
+        return true;
+    }
+}
