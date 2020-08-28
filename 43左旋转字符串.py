58e9a4661459d53e9678bb3dff98dea11b99cc9f
Steve Wang <wanghongzecn@qq.com>
2020-08-28 10:19:32 +0800

    Delete 43左旋转字符串.py

--- 43左旋转字符串.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-class Solution:
-    def LeftRotateString(self, s, n):
-        # write code here
-        length = len(s)
-        if n < 0 or length <= 1:
-            return s
-        n = n%length
-        return s[n:] + s[:n]
