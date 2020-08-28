ba568d970cebb62a50a73315eba3947240b6b9d5
Steve Wang <wanghongzecn@qq.com>
2020-08-28 10:19:04 +0800

    Delete 37数字在排序数组中出现的次数.cpp

--- 37数字在排序数组中出现的次数.cpp
+++
@@ -1,8 +0,0 @@
-class Solution {
-public:
-    int GetNumberOfK(vector<int> data ,int k) {
-        auto l = lower_bound(data.begin(), data.end(), k);
-        auto h = upper_bound(data.begin(), data.end(), k);
-        return h-l;
-    }
-};
