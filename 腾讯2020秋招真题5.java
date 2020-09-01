a173a6e9943ec7b7df7da947e67df75ef80baed5
Steve Wang <wanghongzecn@qq.com>
2020-09-01 16:24:38 +0800

    Update 腾讯2020秋招真题5.java

--- 腾讯2020秋招真题5.java
+++ 腾讯2020秋招真题5.java
@@ -11,7 +11,7 @@ public class Main {
         int n = sc.nextInt();
         int[] a = new int[n];
         int[] b = new int[n];
-        int[][] add = new int[n][n];
+        // int[][] add = new int[n][n];
         for(int i=0; i<n; i++) {
             a[i] = sc.nextInt();
         }
