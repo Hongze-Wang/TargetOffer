3be4e4cf879bec09a3f56ca4d488c8672a11cef3
Steve Wang <wanghongzecn@qq.com>
2020-09-12 13:44:53 +0800

    Update 字节18年算法.cpp

--- 字节18年算法.cpp
+++ 字节18年算法.cpp
@@ -4,8 +4,7 @@ P为给定的二维平面整数点集。定义 P 中某点x，如果x满足 P �
 如下图：实心点为满足条件的点的集合。请实现代码找到集合 P 中的所有 ”最大“ 点的集合并输出。
 */
 
-#incluede <bits/stdc++.h>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 struct Point {
@@ -18,12 +17,12 @@ int main() {
     int n;
     cin >> n;
     for(int i=0; i<n; i++) {
-        scanf("%d%d", &point[i].x, &point[i].y);
+        scanf("%d%d", &points[i].x, &points[i].y);
     }
     sort(points, points+n,
         [](Point &p1, Point &p2) {
           return p1.y==p2.y ? p1.x > p2.x : p1.y < p2.y;
-    })
+    });
     printf("%d %d\n", points[n-1].x, points[n-1].y);
     int maxx = points[n-1].x;
     for(int i=n-2; i>=0; i--) {
