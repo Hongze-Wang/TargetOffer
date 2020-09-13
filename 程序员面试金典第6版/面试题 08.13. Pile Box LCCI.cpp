287939d38e8bb85fc9d0d9d70147e5bcb514145a
Steve Wang <wanghongzecn@qq.com>
2020-09-13 11:18:22 +0800

    Update 面试题 08.13. Pile Box LCCI.cpp

--- 程序员面试金典第6版/面试题 08.13. Pile Box LCCI.cpp
+++ 程序员面试金典第6版/面试题 08.13. Pile Box LCCI.cpp
@@ -38,7 +38,7 @@ public:
         sort(box.begin(), box.end()); // 默认按第一维升序
         int res = 0;
         for(int i=0; i<n; i++) {
-            dp[i] = box[i][2];
+            dp[i] = box[i][2];        // dp数组初始化
             for(int j=0; j<i; j++) {
                 if(box[i][0] > box[j][0] && box[i][1] > box[j][1] && box[i][2] > box[j][2]) {
                     dp[i] = max(dp[i], dp[j] + box[i][2]);
