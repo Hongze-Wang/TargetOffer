6ca57a20324991e5061357998276dc033df4a8cc
Steve Wang <wanghongzecn@qq.com>
2020-09-13 14:16:33 +0800

    Update wenjing09111.py

--- wenjing09111.py
+++ wenjing09111.py
@@ -4,6 +4,10 @@
 
 #假设有多个字符串，现需要对相邻字符串做合并处理，要求如果一个字符串的结尾字符串和下一个字符串的开头字符串相同，
 #那么合并两个字符串（如‘ab’和‘bc’合并后成为‘abc’，‘abc’和‘bc’合并后成为‘abc’）。
+#字符串合并
+
+#假设有多个字符串，现需要对相邻字符串做合并处理，要求如果一个字符串的结尾字符串和下一个字符串的开头字符串相同，
+#那么合并两个字符串（如‘ab’和‘bc’合并后成为‘abc’，‘abc’和‘bc’合并后成为‘abc’）。
 def concat_list(str_list):
     '''
     Input:
@@ -49,13 +53,16 @@ def merge(s1, s2):
         res.append(s1 + s + s2)
     else:
         res.append(str_list[i])
-        
+
+# written after the interview
 def merge_in_place(str_list):
-  for i in range(str_list):
+  idx = 0
+  res = []
+	for i in range(len(str_list)):
     if str_list[i][-1] == str_list[i+1][0]:
       s1, s, s2 = "", "", ""
       l1, l2 = len(len(str_list[i])), len(str_list[i+1])
-      flag = False
+      flag = false
       for j in range(max(l1, l2)):
         if j >= l1:
           s2 += str_list[i+1][j:]
@@ -64,12 +71,16 @@ def merge_in_place(str_list):
           s1 += str_list[i][j]
           break
           
-        if str_list[i][j] == str_list[i+1][j]:
+      	if str_list[i][j] == str_list[i+1][j]:
           flag = True
           s += str_list[i][j]
         if str_list[i] != str_list[i+1] and flag:
           s2 += str_list[i+1][j]
         if str_list[i] != str_list[i+1]:
-          s1 += str_list[i][j]
-      str_list.insert(i, s)
-      str_list.remove(s2)
+					s1 += str_list[i][j]
+      res.insert(idx, s)
+      i += 1
+      idx += 1
+    else:
+      res.insert(idx, str_list[i])
+  return res
